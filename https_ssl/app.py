3f1a045c530513ba4238cb631f03a0e105ee6793
Nyayath Chol <n.chol@alustudent.com>
2026-03-30 20:03:06 +0200

    haproxy ssl termination

---
+++ https_ssl/app.py
@@ -0,0 +1,10 @@
+# app.py
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello HTTPS!"
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=5000)
